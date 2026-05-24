aa41e635f496658e2fc87e91840ddf0393780bdd
Rishikesh Katare <rishikeshkatare20@gmail.com>
2026-05-24 19:13:54 +0530

    Refactor MovementComponent: streamline movement and jump logic, remove unused variables

--- Scripts/Entity/Component/components/movement.gd
+++ Scripts/Entity/Component/components/movement.gd
@@ -2,48 +2,27 @@ class_name MovementComponent
 extends Component
 
 var player : Player
-@export var speed : float
-@export var jump_strength : float
-
+const pS = EntityStats.Stats
 func init(e:Entity) -> void:
 	player = e
-	status = true
-	speed = player.stats.speed
-	jump_strength = player.stats.jump_strength
+	_status = true
 
 func process(delta)->void:
-	if status:
-		if player.velocity.x != 0 and player.velocity.y==0 : player.animator.play("run")
-		else : player.animator.play("idle")
-		
-		if player.velocity.y > 0 : player.animator.play("rise")
-		if player.velocity.y < 0 : player.animator.play("fall")
-		
-		move(delta)
-		jump(delta)
-	else :
+	if not _status:
 		print("Movement Component is disabled")
+		return
+	
+	move(delta)
+	jump(delta)
 
-func move(_delta) -> int:
-	var error_code := 0
-	var dir := Input.get_axis("move_left","move_right")
-	
-	if dir > 0 :
-		player.animator.flip_h = false
-		player.hitbox.scale.x = 1
-	elif dir < 0 : 
-		player.animator.flip_h = true
-		player.hitbox.scale.x = -1
-		
-	player.velocity.x = dir * player.get_speed() 
-	print("Movement direction :", dir, " velocity in x :",player.velocity.x)
-	
-	return error_code
+## Makes the Player Move 
+func move(_delta) -> void:
+	player.velocity.x = player.move_direction * player.get_stat(pS.speed)
+	#print("Movement direction :", player.move_direction, " velocity in x :",player.velocity.x)
 
-func jump(_delta)->int:
-	var error_code := 0
-	if Input.is_action_just_pressed("jump") and player.is_on_floor():
-		player.velocity.y = player.get_jump_strength() * -1 
-		print("Jumped, velocity in y :",player.velocity.y)
+func jump(_delta) -> void:
+	if not ( player.is_jumping and player.is_on_floor() ):
+		return
 	
-	return error_code
+	player.velocity.y = player.get_stat(pS.jump_strength) * -1 
+	#print("Jumped, velocity in y :",player.velocity.y)
