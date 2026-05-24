3e113885649b344ae8dd20f0729f141aa942857b
Rishikesh Katare <rishikeshkatare20@gmail.com>
2026-05-24 19:13:30 +0530

    Refactor player entity: improve input handling and animation logic

--- Scripts/Entity/Component/Entities/player.gd
+++ Scripts/Entity/Component/Entities/player.gd
@@ -1,6 +1,41 @@
 class_name Player
 extends Entity
 
+var is_jumping : bool
+var face_direction : int
+var move_direction : float :
+	set(value):
+		move_direction = value
+		if value == 0 : return
+		else : face_direction = sign(value)
+d
+func _process(_delta: float) -> void:
+	_handle_input()
+	_handle_animation()
+	_handle_face_dirction()
+
+# Handler Functions 
+func _handle_input() -> void:
+	move_direction = Input.get_axis("move_left","move_right")
+	is_jumping = Input.is_action_just_pressed("jump")
+
+func _handle_animation() -> void:
+	if self.velocity.y > 0 : self.animator.play("rise")
+	
+	if self.velocity.y < 0 : self.animator.play("fall")
+	
+	if self.velocity.x != 0 and self.velocity.y==0 : self.animator.play("run")
+	else : self.animator.play("idle")
+
+func _handle_face_dirction() -> void:
+	if face_direction > 0 :
+		self.animator.flip_h = false
+		self.hitbox.scale.x = 1
+	
+	elif face_direction < 0 : 
+		self.animator.flip_h = true
+		self.hitbox.scale.x = -1
+
 func take_damage(_val:float = 0) -> void:
 	var dmg : float = stats.calculate_damage(_val)
 	stats.apply(dmg)
@@ -12,14 +47,15 @@ func die() -> void:
 	self.queue_free()
 	get_tree().reload_current_scene()
 
-func get_health():
-	pass
-# Core Components:
-func get_speed() -> float: 
-	return stats.Base_SPEED
 
-func get_attack() -> float:
-	return stats.Base_ATTACK
 
-func get_jump_strength() -> float:
-	return stats.Base_JUMP_STRENGTH
+# Stats loading 
+func get_stat(stat: EntityStats.Stats) -> float:
+	match stat:
+		EntityStats.Stats.jump_strength:
+			return stats.Base_JUMP_STRENGTH
+		EntityStats.Stats.speed:
+			return stats.Base_SPEED
+		_:
+			push_error("Unknown stat: %s" % stat)
+			return 0.0
